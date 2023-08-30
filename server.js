68e33cb7b0101d20e166502dabcc43699c965302
J-dev740 <tcr19ec029@gectcr.ac.in>
2023-08-30 21:46:13 +0530

    first commit for grpc

---
+++ server.js
@@ -0,0 +1,4 @@
+const grpc= require('grpc')
+//protoLoader compiles your proto file in js chunks which contains your schema
+//here actually the compiler name is ProtoJs
+const protoLoader= require('@grpc/proto-Loader')
